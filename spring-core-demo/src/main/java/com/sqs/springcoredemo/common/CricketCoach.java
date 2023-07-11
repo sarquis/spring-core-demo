a778ee90977be763a9a3e0fc4c88f3a4dcd668ae
rsnf <roberto.sarquis@gmail.com>
2023-07-10 22:01:08 -0300

    Bean Lifecycle Methods

--- spring-core-demo/src/main/java/com/sqs/springcoredemo/common/CricketCoach.java
+++ spring-core-demo/src/main/java/com/sqs/springcoredemo/common/CricketCoach.java
@@ -1,15 +1,35 @@
 package com.sqs.springcoredemo.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
 	System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    /*
+     * Bean Lifecycle Methods
+     * 
+     * Init: method configuration
+     */
+    @PostConstruct
+    public void doMyStartupStuff() {
+	System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    /*
+     * Bean Lifecycle Methods
+     * 
+     * Destroy: method configuration
+     */
+    @PreDestroy
+    public void doMyCleanupStuff() {
+	System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @Override
